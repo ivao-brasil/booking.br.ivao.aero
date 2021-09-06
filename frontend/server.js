d11f140dab546defaef82579e8b81027fe9000df
Luciano <luciano.ssants@gmail.com>
2021-09-06 15:09:26 -0300

    Add workflow

--- frontend/server.js
+++ frontend/server.js
@@ -3,7 +3,7 @@ const next = require("next");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
