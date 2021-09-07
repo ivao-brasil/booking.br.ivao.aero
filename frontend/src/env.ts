48ef8e063918629afc8481ee2aca5e07258683da
Luciano <luciano.ssants@gmail.com>
2021-09-06 22:56:43 -0300

    fix api address

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,3 +1,3 @@
 export const ENV = {
-    API_HOST: process.env.NEXT_PUBLIC_API_HOST || 'https://api.br.ivao.aero'
+    API_HOST: process.env.NEXT_PUBLIC_API_HOST || 'https://bookingapi.br.ivao.aero'
 }
