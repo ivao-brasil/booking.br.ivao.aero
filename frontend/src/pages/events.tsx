7783d9e3290be3fd98112333fb482ed36d90c7d1
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2021-09-07 23:46:35 -0300

    Add home page basic structure

---
+++ frontend/src/pages/events.tsx
@@ -0,0 +1,15 @@
+export default function events() {
+    return (
+        <div>
+            <p>TODO</p>
+            <table>
+                <tr>
+                    <th>Column</th>
+                </tr>
+                <tr>
+                    <td>A</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { ApiClient } from "../clients/api.client";
 import { ENV } from "../env";
 
@@ -5,10 +6,18 @@ export default function index() {
     const apiClient = new ApiClient();
 
     return (
-        <div className="w-screen h-screen" style={{ background: `url(${apiClient.getMainBanner()})` }}>
-            <div>
-                <img src={apiClient.getMainLogo()} className="w-1/5" />
-                {JSON.stringify(ENV)}
+        <div className="w-screen h-screen overflow-hidden" style={{ background: `url(${apiClient.getMainBanner()})` }}>
+            <div className="flex flex-wrap items-center justify-center md:justify-start">
+                <img src={apiClient.getMainLogo()} className="w-48 md:w-1/5" />
+                <Link href="/events" passHref>
+                    <a className="hover:underline">Event list</a>
+                </Link>
+            </div>
+
+            <div className="h-full flex flex-col items-center justify-center">
+                <button className="py-2 px-4 text-white w-48 h-12 font-medium bg-blue-400 shaow-md rounded-md hover:bg-blue-600">
+                    Login
+                </button>
             </div>
         </div>
     )
