7fd5f44bf8e37a403f587b7465e443db0318c5e6
Kewyn Ferreira <kewyn.c@hotmail.com>
2022-01-05 09:58:00 -0300

    Fixes grammar on "Hello, World!"

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ProvideRootContext>
-        <div>Hello World</div>
+        <div>Hello, world!</div>
       </ProvideRootContext>
     </QueryClientProvider>
   </React.StrictMode>,
