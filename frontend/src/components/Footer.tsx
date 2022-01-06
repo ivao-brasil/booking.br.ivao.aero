911bf7f88ac7854ff577ed4ae6c5221ae6190f2f
Luciano Santos <luciano.ssants@gmail.com>
2022-01-05 23:56:39 -0300

    feat: language hook

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,18 +1,52 @@
-import { FunctionComponent } from "react";
 import { FooterText } from "components/typography/Typography";
+import { Language, LanguageDefinition, useLanguage } from "hooks/useLanguage";
+import { FunctionComponent } from "react";
 
-export const Footer: FunctionComponent = () => (
+interface LocalLanguage {
+  privacyPolicy: string;
+  termsOfUse: string;
+}
+
+const LanguageData: LanguageDefinition<LocalLanguage> = {
+  [Language.enUs]: {
+    privacyPolicy: "Privacy Policy",
+    termsOfUse: "Terms of Use",
+  },
+  [Language.ptBr]: {
+    privacyPolicy: "Política de privacidade",
+    termsOfUse: "Termos de uso",
+  },
+};
+
+export const Footer: FunctionComponent = () => {
+  const { languageData } = useLanguage(LanguageData);
+
+  return (
     <footer className="text-center mb-4">
+      <FooterText>
+        © 2021 International Virtual Aviation Organisation - IVAO Brazil.
+      </FooterText>
+      <div>
         <FooterText>
-            © 2021 International Virtual Aviation Organisation - IVAO Brazil.
+          <a
+            className="underline"
+            href="https://wiki.ivao.aero/home/ivao/privacypolicy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {languageData.privacyPolicy}
+          </a>
+          &nbsp; |&nbsp;
+          <a
+            className="underline"
+            href="https://wiki.ivao.aero/home/ivao/terms-of-use"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {languageData.termsOfUse}
+          </a>
         </FooterText>
-        <div>
-            <FooterText>
-                <a className="underline" href="https://wiki.ivao.aero/home/ivao/privacypolicy" target="_blank" rel="noreferrer">Privacy Policy</a>&nbsp;
-                |&nbsp;<a className="underline" href="https://wiki.ivao.aero/home/ivao/terms-of-use" target="_blank" rel="noreferrer">Terms of Use</a>
-            </FooterText>
-        </div>
-
+      </div>
     </footer>
-
-);
+  );
+};
