22cf9db9611455a64f8c9508e9cd19e95dfe8bc0
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2022-02-28 11:44:30 -0300

    Separate API and Admin from main frontend repo

--- backend/routes/airport.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/** @var \Laravel\Lumen\Routing\Router $router */
-$router->group(['middleware' => 'auth', 'prefix' => '/airport'], function() use($router) {
-    $router->get('/details/{icao}', 'AirportController@getDetails');
-});
