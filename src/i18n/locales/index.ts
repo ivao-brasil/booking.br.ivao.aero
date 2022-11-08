ee42c7b803f3686e6a55ac4270f09ede209eafe9
joaotr3ze <117755171+joaotr3ze@users.noreply.github.com>
2022-11-08 13:03:49 -0300

    WIP

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,11 +1,13 @@
 import ptBrTranslations from './pt-br'
 import enUsTranslations from './en-us'
 import frFrTranslations from './fr-fr'
+import esEsTranslations from './es-es'
 
 const resources = {
   'pt-BR': ptBrTranslations,
   'en-US': enUsTranslations,
-  'fr-FR': frFrTranslations
+  'fr-FR': frFrTranslations,
+  'es-ES': esEsTranslations
 }
 
 export default resources
