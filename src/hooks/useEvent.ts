c2da13ce2985da7575cf1882ee02258ad430fb87
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-05-29 00:37:34 -0300

    fix: fix events

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -1,36 +1,19 @@
-import { useContext } from "react";
-import { AxiosError } from "axios";
-import { InfiniteData, useQuery, useQueryClient } from "react-query";
-import { ONE_DAY } from "appConstants";
-import { IocContext } from "context/IocContext";
-import { Event } from "types/Event";
-import { Pagination } from "types/Pagination";
+import {useContext} from "react";
+import {AxiosError} from "axios";
+import {useQuery} from "react-query";
+import {ONE_DAY} from "appConstants";
+import {IocContext} from "context/IocContext";
+import {Event} from "types/Event";
 
 export function useEvent(id: number) {
-	const { apiClient } = useContext(IocContext);
-	const queryClient = useQueryClient();
-	const EVENT_LIST_KEY = "events";
+  const {apiClient} = useContext(IocContext);
+  const EVENT_DETAIL_KEY = ["event", id];
 
-	const eventData = useQuery<Event, AxiosError>(["event", id], async () => await apiClient.getEvent(id),
-		{
-			staleTime: ONE_DAY,
-			initialData: () => {
-				const eventListQuery = queryClient.getQueryData<InfiniteData<Pagination<Event>>>(EVENT_LIST_KEY);
-				let fetechedEvent = undefined;
+  const eventData = useQuery<Event, AxiosError>(EVENT_DETAIL_KEY, async () => await apiClient.getEvent(id),
+    {
+      staleTime: ONE_DAY
+    });
 
-				eventListQuery?.pages.forEach(page => {
-					page.data.forEach(event => {
-						if (event.id === id) {
-							fetechedEvent = event;
-						}
-					});
-				});
-
-				return fetechedEvent;
-			},
-			initialDataUpdatedAt: () => queryClient.getQueryState(EVENT_LIST_KEY)?.dataUpdatedAt
-		});
-
-	return eventData;
+  return eventData;
 
 }
