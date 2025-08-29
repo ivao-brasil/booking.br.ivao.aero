07c82fb665d403c46aaa2f3e174cf12e92b595a4
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-08-28 23:21:09 -0300

    fix(env): update authorization server URL

--- src/env.ts
+++ src/env.ts
@@ -7,7 +7,7 @@ let envVariables = {
     LOGO_SIDEBAR_LIGHT: process.env.REACT_APP_LOGO_SIDEBAR_LIGHT || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
     LOGO_SIDEBAR_DARK: process.env.REACT_APP_LOGO_SIDEBAR_DARK || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
     ANALYTICS_TRACKING_ID: process.env.REACT_APP_ANALYTICS_TRACKING_ID || '',
-    AUTHORIZATION_SERVER: process.env.REACT_APP_AUTHORIZATION_SERVER || 'https://login.ivao.aero/index.php'
+    AUTHORIZATION_SERVER: process.env.REACT_APP_AUTHORIZATION_SERVER || 'https://sso.ivao.aero/authorize'
 };
 
 if (activeEnv === 'development') {
