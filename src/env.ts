6dad558c6cc37835037e147cfee9a30dc589e50d
Bruno Ricci <brunoriccidossantos@hotmail.com>
2025-09-28 19:36:06 -0300

    feat(auth): integrate CLIENT_ID into login flow

--- src/env.ts
+++ src/env.ts
@@ -1,26 +1,26 @@
 const activeEnv = process.env.NODE_ENV || 'production';
 
 let envVariables = {
-  IVAO_KRONOS_API_SERVER: process.env.REACT_APP_API_HOST || 'http://localhost.ivao.aero:8080/api',
   LOGO_LIGHT: process.env.REACT_APP_LOGO_LIGHT || 'https://assets.br.ivao.aero/logos/kronos_light.svg',
   LOGO_DARK: process.env.REACT_APP_LOGO_DARK || 'https://assets.br.ivao.aero/logos/kronos_dark.svg',
   LOGO_SIDEBAR_LIGHT: process.env.REACT_APP_LOGO_SIDEBAR_LIGHT || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
   LOGO_SIDEBAR_DARK: process.env.REACT_APP_LOGO_SIDEBAR_DARK || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
   ANALYTICS_TRACKING_ID: process.env.REACT_APP_ANALYTICS_TRACKING_ID || '',
-  IVAO_API_SERVER: process.env.IVAO_API_SERVER || 'https://api.ivao.aero',
-  CLIENT_ID: process.env.REACT_APP_CLIENT_ID || '',
+  IVAO_KRONOS_API_SERVER: process.env.IVAO_KRONOS_API_HOST || '',
+  IVAO_API_SERVER: process.env.IVAO_API_HOST || 'https://api.ivao.aero',
+  CLIENT_ID: process.env.CLIENT_ID || '',
 };
 
 if (activeEnv === 'development') {
   envVariables = {
-    IVAO_KRONOS_API_SERVER: process.env.REACT_APP_API_HOST || 'http://localhost:3003/api',
     LOGO_LIGHT: process.env.REACT_APP_LOGO_LIGHT || 'https://assets.br.ivao.aero/logos/kronos_light.svg',
     LOGO_DARK: process.env.REACT_APP_LOGO_DARK || 'https://assets.br.ivao.aero/logos/kronos_dark.svg',
     LOGO_SIDEBAR_LIGHT: process.env.REACT_APP_LOGO_SIDEBAR_LIGHT || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
     LOGO_SIDEBAR_DARK: process.env.REACT_APP_LOGO_SIDEBAR_DARK || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
     ANALYTICS_TRACKING_ID: process.env.REACT_APP_ANALYTICS_TRACKING_ID || '',
-    IVAO_API_SERVER: process.env.IVAO_API_SERVER || 'http://localhost:3003',
-    CLIENT_ID: process.env.REACT_APP_CLIENT_ID || '',
+    IVAO_KRONOS_API_SERVER: process.env.IVAO_KRONOS_API_HOST || 'http://localhost:3003/api',
+    IVAO_API_SERVER: process.env.IVAO_API_HOST || 'http://localhost:3003',
+    CLIENT_ID: process.env.CLIENT_ID || '',
   }
 }
 
